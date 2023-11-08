d6a253cefb8e9487ee79621379b9550e66373aa6
0xFloop <floopcrypto@yahoo.com>
2023-11-08 15:36:24 -0500

    supply_stack part two

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod rock_paper_scissors;
 mod rucksack_reorganization;
 mod supply_stacks;
 fn main() {
-    let your_num = supply_stacks::get_top_crates();
+    let your_num = supply_stacks::get_top_crates_part_two();
     print!("Your num: {}", your_num);
 }
--- src/supply_stacks.rs
+++ src/supply_stacks.rs
@@ -85,3 +85,95 @@ pub fn get_top_crates() -> u32 {
 
     return 3;
 }
+
+
+pub fn get_top_crates_part_two() -> u32 {
+    let mut file = File::open("utils/day_5_input.txt").expect("File not found");
+
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
+
+    let instructions: Vec<&str> = contents.split('\n').collect();
+
+    let mut stacks: Vec<Vec<char>> = Vec::new();
+
+    let num_of_stacks = 9;
+
+    for _ in 0..num_of_stacks {
+        stacks.push(Vec::new());
+    }
+
+    let mut isolated_chars: Vec<char> = Vec::new();
+
+    for instruction in &instructions {
+        if instruction == &"" {
+            continue;
+        }
+        if !instruction.starts_with("move") {
+            //this is a setup stacks instruction
+            if !(instruction.contains("1")) {
+                for i in (0..instruction.len()).step_by(4) {
+                    let instruction_chars: Vec<_> = instruction.chars().collect();
+                    let current_char: &[char];
+
+                    if i + 4 > instruction.len() {
+                        current_char = &instruction_chars[i..instruction.len()];
+                    } else {
+                        current_char = &instruction_chars[i..i + 4];
+                    }
+
+                    isolated_chars.push(current_char[1]);
+                }
+                continue;
+            }
+        }
+    }
+    for i in 0..isolated_chars.len() {
+        let current_isolated_char = isolated_chars[i];
+        if current_isolated_char == ' ' {
+            continue;
+        }
+        stacks[i % 9].push(current_isolated_char);
+    }
+    for stack in stacks.iter_mut() {
+        stack.reverse();
+    }
+
+    for instruction in instructions {
+        if instruction == "" {
+            continue;
+        }
+        if !instruction.starts_with("move") {
+            continue;
+        }
+        let instruction_split: Vec<_> = instruction.split(" ").collect();
+
+        let num_to_move = instruction_split[1].parse::<u32>().unwrap();
+        let from_stack = instruction_split[3].parse::<u32>().unwrap() - 1;
+        let to_stack = instruction_split[5].parse::<u32>().unwrap() - 1;
+
+        let mut current_move_holder:Vec<char> = Vec::new();
+
+        for i in 0..num_to_move {
+            current_move_holder.push(stacks[from_stack as usize].pop().unwrap());
+        }
+        
+        for i in 0..num_to_move {
+            stacks[to_stack as usize].push(current_move_holder.pop().unwrap());
+        }
+
+
+    }
+
+
+    let mut final_letters: Vec<&char> = Vec::new();
+
+    for stack in stacks.iter_mut() {
+        final_letters.push(stack.last().unwrap());
+    }
+    println!("{:?}", final_letters);
+
+    return 3;
+}
